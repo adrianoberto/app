799a69aef1a630ce2aa3b712bf09b79fbf15a51c
Adriano Berto <adriano.berto@outlook.com>
2021-09-19 13:17:17 -0300

    renomeação projeto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'financeiro'`, () => {
+  it(`should have as title 'app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('financeiro');
+    expect(app.title).toEqual('app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('financeiro app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('app app is running!');
   });
 });
