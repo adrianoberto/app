b40ca4218ff3aad6e702019758c7f55e8e610dc4
Adriano Berto <adriano.berto@outlook.com>
2021-11-12 19:07:07 -0300

    inclusao de firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,  
-  baseUrl: 'https://berto-financeiro.herokuapp.com'
+  
 };
 
 /*
